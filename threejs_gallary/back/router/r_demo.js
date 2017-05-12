0bbea82f89653edffbee14feaec035f6d25783df
chenjsh36 <chenjsh36@outlook.com>
2017-05-12 17:02:48 +0800

    physics ammo.js

--- threejs_gallary/back/router/r_demo.js
+++ threejs_gallary/back/router/r_demo.js
@@ -10,6 +10,7 @@ app.set('views', path.join(__dirname, './../../front/dist/view'));
 
 app.get('/:filename', function(req, res) {
     var filename = req.params.filename;
+    console.log('req ', req.params.filename)
     res.render(filename, {
         title: 'Demo' + req.params.filename
     });
